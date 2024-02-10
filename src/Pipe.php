7df68b10b77f9508448f2cdba645be186711dba0
3lvir4 <pierreavril18@gmail.com>
2024-02-10 07:17:39 +0100

    docs(Pipe): initial docs

--- src/Pipe.php
+++ src/Pipe.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace Elvir4\FunFp;
 
 use Elvir4\FunFp\Contracts\ProvidesIterOps;
+use function Elvir4\FunFp\constructors\pipe;
 
 /**
+ * Represents a chain of operations.
+ *
+ * @see pipe()
+ * @link https://en.wikipedia.org/wiki/Pipeline_(software)
  * @psalm-suppress MixedAssignment
  * @implements ProvidesIterOps<int, callable>
  */
@@ -20,6 +25,7 @@ final class Pipe implements ProvidesIterOps
     ) {}
 
     /**
+     * @see pipe()
      * @param mixed ...$args
      * @return mixed
      */
@@ -28,6 +34,12 @@ final class Pipe implements ProvidesIterOps
         return $this->eval(...$args);
     }
 
+    /**
+     * Adds a callable to the pipeline.
+     *
+     * @param callable $f
+     * @return $this
+     */
     public function then(callable $f): self
     {
         $this->fns[] = $f;
@@ -35,6 +47,8 @@ final class Pipe implements ProvidesIterOps
     }
 
     /**
+     * Evaluates the pipeline with the provided arguments as initial input.
+     *
      * @param mixed ...$args
      * @return mixed
      */
